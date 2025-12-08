923b8d3cf108c732eeb71be7a676c166945326d7
Ben Robertson <ben@robertson.is>
2025-12-07 19:44:39 -0500

    SEO updates.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwind()],
-  },
+    site: 'https://marcoswift.com',
+    integrations: [sitemap()],
+    vite: {
+        plugins: [tailwind()],
+    },
 });
+
